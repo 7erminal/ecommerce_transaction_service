cbd984bb314149087a133574d5c71223631f895e
Terminal-Adawe <bede.abbe91@gmail.com>
2026-04-17 14:36:58 +0000

    changes

--- controllers/transactions_v2.go
+++ controllers/transactions_v2.go
@@ -291,13 +291,13 @@ func (c *TransactionsV2Controller) UserPost() {
 						// Save in user_ins_transactions table to add details in the steps
 						userInsTransaction := models.UserInsTransactions{
 							UserInsTransactionId:   "TRX-INS-" + strconv.FormatInt(time.Now().Unix(), 10) + strconv.FormatInt(v.RequestId, 10),
-							UserTransactionId:      &transaction,
+							UserTransactionId:      &models.UserTransactions{TransactionId: transaction.TransactionId},
 							Amount:                 req.Amount,
 							Data:                   "",
 							SenderAccountNumber:    req.SourceAccountNumber,
 							RecipientAccountNumber: req.DestinationAccountNumber,
-							Service:                service,
-							Status:                 status,
+							Service:                &models.Services{ServiceId: service.ServiceId},
+							Status:                 &models.Status_codes{StatusId: status.StatusId},
 							Request:                string(reqText),
 							Response:               "",
 							DateCreated:            time.Now(),
