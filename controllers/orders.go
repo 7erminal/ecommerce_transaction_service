9febed0f2677281b6f38a6a89175093f2ecc1613
Terminal-Adawe <bede.abbe91@gmail.com>
2026-07-26 12:09:32 +0000

    created by column type change

--- controllers/orders.go
+++ controllers/orders.go
@@ -852,7 +852,7 @@ func (c *OrdersController) GetAll() {
 	var sortby []string
 	var order []string
 	var query = make(map[string]string)
-	var limit int64 = 100
+	var limit int64 = 30
 	var offset int64
 
 	// fields: col1,col2,entity.col3
